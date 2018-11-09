70cb306d088cebd6df6ac4b6ea83acc846861558
dima <dima@localhost.localdomain>
2018-11-09 16:39:26 +0200

    fix token

--- comments/middleware.py
+++ comments/middleware.py
@@ -15,6 +15,6 @@ class AuthMiddleware(object):
     def process_request(self, req, resp):
         if 'X-NEMO-AUTH' not in req.headers:
             raise falcon.HTTPUnauthorized('Missing auth token')
-        auth_token = req.headers.get('X-AUTH-NEMO')
+        auth_token = req.headers.get('X-NEMO-AUTH')
         user_identity = jwt.decode(auth_token, 'secret')
         req.context['user_identity'] = user_identity
